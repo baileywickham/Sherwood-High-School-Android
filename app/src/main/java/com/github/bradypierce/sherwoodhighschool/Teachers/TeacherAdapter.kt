91156853833864aa1b0e972ab0a71f70da4f66fa
brady pierce <bradypierce13@gmail.com>
2016-09-11 22:43:20 -0700

    Teacher Overflow Menu

--- app/src/main/java/com/github/bradypierce/sherwoodhighschool/Teachers/TeacherAdapter.kt
+++ app/src/main/java/com/github/bradypierce/sherwoodhighschool/Teachers/TeacherAdapter.kt
@@ -1,7 +1,9 @@
 package com.github.bradypierce.sherwoodhighschool.Teachers
 
+import android.support.v7.widget.PopupMenu
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.github.bradypierce.sherwoodhighschool.Model.Teacher.Teacher
 import com.github.bradypierce.sherwoodhighschool.R
@@ -12,10 +14,13 @@ import com.github.bradypierce.sherwoodhighschool.R
 
 class TeacherAdapter(teachers: List<Teacher>?): RecyclerView.Adapter<TeacherViewHolder>() {
 
+    lateinit var popupMenu: PopupMenu
     val teachers = teachers
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TeacherViewHolder {
-        return TeacherViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.item_teacher_view, parent, false))
+        var view: View = LayoutInflater.from(parent?.context).inflate(R.layout.item_teacher_view, parent, false)
+        var viewHolder = TeacherViewHolder(view, parent!!.context)
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: TeacherViewHolder?, position: Int) {
--- app/src/main/java/com/github/bradypierce/sherwoodhighschool/Teachers/TeacherViewHolder.kt
+++ app/src/main/java/com/github/bradypierce/sherwoodhighschool/Teachers/TeacherViewHolder.kt
@@ -1,5 +1,7 @@
 package com.github.bradypierce.sherwoodhighschool.Teachers
 
+import android.content.Context
+import android.support.v7.widget.PopupMenu
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageButton
@@ -12,8 +14,10 @@ import com.github.bradypierce.sherwoodhighschool.Utils.bindView
  * Created by bradypierce on 9/11/16.
  */
 
-class TeacherViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class TeacherViewHolder(view: View, context: Context): RecyclerView.ViewHolder(view) {
 
+    lateinit var popupMenu: PopupMenu
+    val context = context
     val teacherName: TextView by bindView(R.id.item_teacher_name)
     val teacherPosition: TextView by bindView(R.id.item_teacher_position)
     val teacherOverflow: ImageButton by bindView(R.id.item_teacher_overflow)
@@ -21,6 +25,28 @@ class TeacherViewHolder(view: View): RecyclerView.ViewHolder(view) {
     fun bind(teacher: Teacher) {
         teacherName.text = teacher.name
         teacherPosition.text = teacher.position
+
+        popupMenu = PopupMenu(context, teacherOverflow)
+        popupMenu.menuInflater.inflate(R.menu.menu_teacher_overflow, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.teacher_overflow_email -> {
+
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.teacher_overflow_phone -> {
+
+                    return@setOnMenuItemClickListener true
+                }
+                else -> {
+                    return@setOnMenuItemClickListener false
+                }
+            }
+        }
+
+        teacherOverflow.setOnClickListener {
+            popupMenu.show()
+        }
     }
 
 }
